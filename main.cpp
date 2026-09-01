bf2f05cfacdbdd717545c8712022d12d7823ea3d
max <max.koeg@gmail.com>
2026-09-01 15:06:40 -0700

    struct commit

--- main.cpp
+++ main.cpp
@@ -6,7 +6,10 @@ using namespace std;
 
 
 struct Restaurant {
-    int = rating;
-    string 
+    int rating;
+    string address;
+    string cuisine;
+    bool openStatus;
+    double waitTime;
+};
 
-}
