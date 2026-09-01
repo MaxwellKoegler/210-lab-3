b4676b698b29e792430536e6869ea6ad48401d05
max <max.koeg@gmail.com>
2026-09-01 15:12:49 -0700

    struct test

--- main.cpp
+++ main.cpp
@@ -3,8 +3,6 @@
 
 using namespace std;
 
-
-
 struct Restaurant {
     int rating;
     string address;
@@ -13,3 +11,19 @@ struct Restaurant {
     double waitTime;
 };
 
+int main() {
+    Restaurant r1;
+    r1.rating = 4;
+    r1.address = "123 simple st.";
+    r1.cuisine = "french";
+    r1.openStatus = false;
+    r1.waitTime = 0.0;
+
+    cout << r1.rating << endl;
+    cout << r1.address << endl;
+    cout << r1.cuisine << endl;
+    cout << r1.openStatus << endl;
+    cout << r1.waitTime << endl;
+
+
+}
