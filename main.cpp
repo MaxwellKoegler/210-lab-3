09de3e0d4cce568814486c7d6178efaffb7b19a8
max <max.koeg@gmail.com>
2026-09-01 15:37:28 -0700

    Milestone 2

--- main.cpp
+++ main.cpp
@@ -17,7 +17,17 @@ void present(Restaurant);
 
 int main() {
     Restaurant r1 = populate();
+    cout << "Restaurant 1: " << endl;
     present(r1);
+    Restaurant r2 = populate();
+    cout << "Restaurant 2: " << endl;
+    present(r2);
+    Restaurant r3 = populate();
+    cout << "Restaurant 3: " << endl;
+    present(r3);
+    Restaurant r4 = populate();
+    cout << "Restaurant 4: " << endl;
+    present(r4);
     exit(0);
 }
 
